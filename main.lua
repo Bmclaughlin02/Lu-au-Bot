ab1449c15a03a981af78702e1dce9686451e579f
AidenG03 <agallo22@uncc.edu>
2023-03-20 22:11:21 -0400

    modifying main lua file with intial code for Lu'au Bot

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
---Inital code/main file
+-- Inital code/main file -- 
+
+local discordia = require('discordia')
+local client = discordia.Client()
+
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+-- pong me if I ping you
+client:on('messageCreate', function(message)
+	if message.content == '!ping' then
+		message.channel:send('Pong!')
+	end
+end)
+
+-- call the token
+client:run('Bot ' .. token)
