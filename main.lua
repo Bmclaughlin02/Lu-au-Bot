0bc6521dff0a0fdf146f941a2f01a6797ed38e45
BrennanMcl <bdmclaughlin02@gmail.com>
2023-03-21 14:16:57 -0400

    Edited ignore, checked around in main to get bot working

--- main.lua
+++ main.lua
@@ -1,17 +1,21 @@
 -- Inital code/main file -- 
+local discord = require('discordia')
+local client = discord.Client()
 
-local discordia = require('discordia')
-local client = discordia.Client()
-
+file = io.open('config.txt', 'r')
+token = file:read()
+file:close()
 
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
+	print('Bot token ' .. token)
 end)
 
 -- pong me if I ping you
 client:on('messageCreate', function(message)
 	if message.content == '!ping' then
-		message.channel:send('Pong!')
+		print('Bug test, message was read')
+		message:reply('Pong!')
 	end
 end)
 
