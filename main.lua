ca7ddc0cd9ec90614f3e416b88fd34c823041730
BrennanMcl <bdmclaughlin02@gmail.com>
2023-03-22 09:51:39 -0400

    Added experimental, Made command function

--- main.lua
+++ main.lua
@@ -12,8 +12,22 @@ end)
 
 -- pong me if I ping you
 client:on('messageCreate', function(message)
-	if message.content == '!ping' then	
+	if message.content == '!ping' then
 		message:reply('Pong!')
+	end
+end)
+
+-- Simple experimental function using message read and if statements
+client:on('messageCreate', function(message)
+	postBody = message.content:lower()
+	isCommand = postBody:sub(1, 1) == '!'
+
+	if not isCommand then
+		return
+	end
+
+	if postBody:sub(2) == 'hello' then
+		message:reply("Hi I am Lu'au Bot")
 	end
 end)
 
