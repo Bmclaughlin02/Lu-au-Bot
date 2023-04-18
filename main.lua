7019e9db81146d836bc59ffe9e35d5aa981be887
BrennanMcl <bdmclaughlin02@gmail.com>
2023-04-18 16:54:26 -0400

    began work on music function

--- main.lua
+++ main.lua
@@ -9,6 +9,50 @@ file = io.open('config.txt', 'r')
 token = file:read()
 file:close()
 
+function split(input, sep)
+	local t = {}
+
+	-- If no seperator specified uses spaces
+	if sep == nil then
+		sep = '%s'
+	end
+	-- Iterates through return from gmatch and loads them into the return table
+	for str in string.gmatch(input, '([^'..sep..']+)') do
+		table.insert(t, str)
+	end
+
+	return t
+end
+
+function fileExists(name)
+	local f=io.open(name,"r")
+	if f~=nil then io.close(f) return true else return false end
+end
+
+function getVideo(vid)
+	audioFile = 'audio.mp3'
+
+	print('Fetching video')
+	if fileExists(audioFile) then
+		os.remove(audioFile)
+	end
+
+	os.execute("powershell -file downloader.ps1 " .. vid)
+
+	while true do
+		print('downloading...')
+		if fileExists(audioFile) then
+			break
+		else
+			local start = os.time()
+			repeat until os.time() > start + 0.125
+		end
+	end
+
+	print('Done')
+	return audioFile
+end
+
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
@@ -32,13 +76,28 @@ client:on('messageCreate', function(message)
 
 	local postBody = message.content:lower()
 	local isCommand = postBody:sub(1, 1) == '!'
+	postBody = postBody:sub(2)
 
 	if not isCommand then
 		return
 	end
 
-	if postBody:sub(2) == 'hello' then
+	print(postBody)
+	local command = split(postBody, ' ')
+
+	if command[1] == 'hello' then
 		message:reply("Hi I am Lu'au Bot")
+	elseif command[1] == 'play' then
+		table.remove(command, 1)
+		local vid = table.concat(command, ' ')
+		local vc = client:getChannel('1077675032370745520')
+		local connection = vc:join()
+		print('making call to get video')
+		local stream = getVideo(vid)
+		coroutine.wrap(function()
+			connection:playFFmpeg(stream)
+		end)()
+
 	end
 end)
 
