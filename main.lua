2b4c124b1c36b9f7b0059c860014549609c24c6e
BrennanMcl <bdmclaughlin02@gmail.com>
2023-04-24 11:06:23 -0400

    Added reaction command from Nykyta

--- main.lua
+++ main.lua
@@ -44,6 +44,7 @@ function getAudio(audio)
 	return './/src//'..audioFile
 end
 
+-- Contributed by Rachel
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
@@ -51,6 +52,7 @@ end)
 client:on('memberJoin', function(member)
 	print('I am being called properly')
     local WelcomeMessage = string.format("Hello %s, Welcome to our server!", member.username)
+	-- COPY BOT COMMAND CHANNEL ID HERE IN THE getChannel FIELD
     client:getChannel('1099767452495249428'):send(WelcomeMessage)
 end)
 
@@ -59,6 +61,7 @@ client:on('messageCreate', function(message)
 		message:reply('Pong!')
 	end
 
+	--This was contributed by Darius
 	local forbiddenWords = {'fuck', 'shit', 'ass', 'nigger', 'nigga', 'faggot', 'bitch'}
 
 	for k, v in ipairs(forbiddenWords) do
@@ -86,8 +89,10 @@ client:on('messageCreate', function(message)
 		message:reply("Hi I am Lu'au Bot")
 	end
 		
+	-- Command contributed by Brennan McLaughlin
 	if command[1] == 'play' then
 		local audio = command[2]
+		-- COPY VC FOR MUSIC COMMANDS HERE IN THE getChannel FIELD
 		local channel  = client:getChannel('1077675032370745520')
 		local connection = channel:join()
 		print('making call to get audio')
@@ -100,6 +105,7 @@ client:on('messageCreate', function(message)
 		end
 	end
 
+	-- Contributed by Aiden
 	if command[1] == 'weather' then
 		local city = command[2]
 		os.execute('python weather.py ' .. city)
@@ -114,6 +120,38 @@ client:on('messageCreate', function(message)
 		end
 
 		message:reply({file = 'report.txt'})
+	end
+
+	-- Method contributed by Nykyta
+	if command[1] == 'reaction' then
+		local x = math.random(10)
+		local randImage = ""
+
+		if x == 0 then
+			randImage = "1.gif"
+		elseif x == 1 then
+			randImage = "2.jfif"
+		elseif x == 2 then
+			randImage = "3.gif"
+		elseif x == 3 then
+			randImage = "4.gif"
+		elseif x == 4 then
+			randImage = "5.jpg"
+		elseif x == 5 then
+			randImage = "6.jfif"
+		elseif x == 6 then
+			randImage = "7.jfif"
+		elseif x == 7 then
+			randImage = "8.gif"
+		elseif x == 8 then
+			randImage = "9.gif"
+		elseif x == 9 then
+			randImage = "10.gif"
+		elseif x == 10 then
+			randImage = "11.gif"
+		end
+
+		message:reply({file = './/src//reactions//'..randImage})
 	end
 end)
 
